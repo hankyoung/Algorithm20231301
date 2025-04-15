d4eb727c724ac74a8dcab8226ee3062d8d09cbfb
Kevin Stratvert <harrylee2004@naver.com>
2025-04-15 15:30:24 +0900

    changed vector functions and classic algorithms so stability can be confirmed

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -7,40 +7,30 @@
 #include <climits>
 #include <tuple>
 
-std::vector<int> make_random_vector(int size) { //generate vector of random size
-    std::vector<int> input_list(size);
-    for (int& x : input_list) {
-        x = rand() % 1000001;  // random number from 0~10^6
+std::vector<std::pair<int, int>> make_random_vector(int size) { //generate vector of random size
+    std::vector<std::pair<int, int>> input_list(size);
+    for (int i = 0; i < size; i++) {
+        int value = rand() % 1000001;
+        input_list[i] = {value, i};  
     }
     return input_list;
 }
 
-std::vector<int> make_ordered_vector(int size) { //generate ordered vector
+std::vector<std::pair<int, int>> make_ordered_vector(int size) { //generate ordered vector
     int i = 1;
-    std::vector<int> input_list(size);
-    for (int& x : input_list) {
-        x = i;
-        i++;
+    std::vector<std::pair<int, int>> input_list(size);
+    for (int i = 0; i < size; i++) {
+        int value = rand() % 1000001;
+        input_list[i] = {value, 0};  
     }
     return input_list;
 }
 
-bool is_sorted(std::vector<int>& list) {
-    for(int i = 0; i < list.size() - 1; i++) {
-        if (list[i] > list[i+1]) {
-            std::cout << "error" << std::endl;
-            return false;
-        }
-    }
-    std::cout << "sorted!" << std::endl;
-    return true;
-}
-
-void bubble_sort(std::vector<int>& list) { 
+void bubble_sort(std::vector<std::pair<int, int>>& list) { 
     bool is_swapped = false;
     for(int times = 0; times < list.size()-1; times++) {
         for(int i = 0; i < list.size() - 1; i++) {
-            if(list[i] > list[i+1]) {
+            if(list[i].first > list[i+1].first) {
                 std::swap(list[i], list[i+1]);
                 is_swapped = true;
             }
@@ -51,40 +41,39 @@ void bubble_sort(std::vector<int>& list) {
     }
 } 
 
-void selection_sort(std::vector<int>& list) {
-    int start = 0;
-    while(start < list.size()) {
+void selection_sort(std::vector<std::pair<int, int>>& list) {
+    for (int start = 0; start < list.size() - 1; start++) {
         int min_index = start;
-        int min = INT_MAX;
-        for(int i = start; i < list.size(); i++) {
-            if (list[i] < min) {
-                min = list[i];      
+        for (int i = start + 1; i < list.size(); i++) {
+            if (list[i].first < list[min_index].first) {
                 min_index = i;
             }
         }
-        std::swap(list[start], list[min_index]);
-        start++;
+        if (min_index != start) {
+            std::swap(list[start], list[min_index]);
+        }
     }
 }
 
-void insertion_sort(std::vector<int>& list) {
-    int key = 1;
-    while(key < list.size()) {
-        int i = key;
-        while(i >= 1 && list[i] < list[i-1]) {
-            std::swap(list[i], list[i-1]);
+void insertion_sort(std::vector<std::pair<int, int>>&  list) {
+    for (int key = 1; key < list.size(); key++) {
+        int i = key - 1;
+        std::pair<int, int> value = list[key];
+
+        while(i >= 0 && list[i].first > value.first) {
+            list[i + 1] = list[i];
             i--;
         }
-        key++;
+        list[i + 1] = value; 
     }
 }
 
-std::vector<int> merge(std::vector<int> left, std::vector<int> right) {
+std::vector<std::pair<int, int>> merge(std::vector<std::pair<int, int>> left, std::vector<std::pair<int, int>> right) {
     int i = 0, j = 0;
-    std::vector<int> merged_vector;
+    std::vector<std::pair<int, int>> merged_vector;
 
     while(i < left.size() && j < right.size()) {
-        if (left[i] <= right[j]) {
+        if (left[i].first < right[j].first || (left[i].first == right[j].first && left[i].second < right[j].second)) {
             merged_vector.push_back(left[i]);
             i++;
         }
@@ -106,9 +95,9 @@ std::vector<int> merge(std::vector<int> left, std::vector<int> right) {
     return merged_vector;
 }
 
-void merge_sort(std::vector<int>& list) {
-    std::vector<int> left;
-    std::vector<int> right;
+void merge_sort(std::vector<std::pair<int, int>>& list) {
+    std::vector<std::pair<int, int>> left;
+    std::vector<std::pair<int, int>> right;
 
     if (list.size() <= 1) {
         return;
@@ -125,21 +114,21 @@ void merge_sort(std::vector<int>& list) {
 
     merge_sort(left);
     merge_sort(right);
-    std::vector<int> merged_vector = merge(left, right);
+    std::vector<std::pair<int, int>> merged_vector = merge(left, right);
     list = merged_vector;
 }
 
-std::tuple<std::vector<int>, std::vector<int>, std::vector<int>> quick_partition(std::vector<int> list) {
-    std::vector<int> small;
-    std::vector<int> big;
-    std::vector<int> equal;
-    int pivot = list[rand() % list.size()];
+std::tuple<std::vector<std::pair<int, int>>, std::vector<std::pair<int, int>>, std::vector<std::pair<int, int>>> quick_partition(std::vector<std::pair<int, int>> list) {
+    std::vector<std::pair<int, int>> small;
+    std::vector<std::pair<int, int>> big;
+    std::vector<std::pair<int, int>> equal;
+    int pivot = list[rand() % list.size()].first;
 
-    for(int& x : list) {
-        if (x < pivot) {
+    for(std::pair<int, int>& x : list) {
+        if (x.first < pivot) {
             small.push_back(x);
         }
-        else if (x > pivot) {
+        else if (x.first > pivot) {
             big.push_back(x);
         }
         else {
@@ -149,35 +138,35 @@ std::tuple<std::vector<int>, std::vector<int>, std::vector<int>> quick_partition
     return std::make_tuple(small, equal, big);
 }
 
-void quick_sort(std::vector<int>& list) {
+void quick_sort(std::vector<std::pair<int, int>>& list) {
     if (list.size() <= 1) {
         return;
     }
-    std::vector<int> small, equal, big;
+    std::vector<std::pair<int, int>> small, equal, big;
     std::tie(small, equal, big) = quick_partition(list);
     quick_sort(small);
     quick_sort(big);
     list.clear();
 
-    for (int& x: small) {
+    for (std::pair<int, int>& x: small) {
         list.push_back(x);
     }
 
-    for (int& x: equal) {
+    for (std::pair<int, int>& x: equal) {
         list.push_back(x);
     }
 
-    for (int& x: big) {
+    for (std::pair<int, int>& x: big) {
         list.push_back(x);
     }
 }
 
-void heapify(std::vector<int>& list, int i, int size) {
+void heapify(std::vector<std::pair<int, int>>& list, int i, int size) {
     int max_index = i;
-    if (2 * i + 1 < size && list[max_index] < list[2 * i + 1]) {
+    if (2 * i + 1 < size && list[max_index].first < list[2 * i + 1].first) {
         max_index = 2 * i + 1;
     }
-    if (2 * i + 2  < size && list[max_index] < list[2 * i + 2]) {
+    if (2 * i + 2  < size && list[max_index].first < list[2 * i + 2].first) {
         max_index = 2 * i + 2;
     }
 
@@ -187,13 +176,13 @@ void heapify(std::vector<int>& list, int i, int size) {
     }
 }
 
-void build_max_heap(std::vector<int>& list, int size) {
+void build_max_heap(std::vector<std::pair<int, int>>& list, int size) {
     for(int i = (size / 2) - 1 ; i >= 0; i--) {
         heapify(list, i, size);
     }
 }
 
-void heap_sort(std::vector<int>& list) {
+void heap_sort(std::vector<std::pair<int, int>>& list) {
     int size = list.size();
     build_max_heap(list, size);
     while (size > 1) {
@@ -385,20 +374,131 @@ std::vector<int> build_tree(std::vector<int>& list) {
     return tree;
 } 
 
+void remove_winner(std::vector<int>& tree) {
+    int winner = tree[0];
+    int start = (tree.size() + 1) / 2 - 1;
+    int index = INT_MIN;
+
+    for(int i = start; i < tree.size(); i++) {
+        if(tree[i] == winner) {
+            tree[i] = INT_MAX;
+            index = i;
+            break;
+        }
+    }
+
+    if(index == INT_MIN) {
+        return;
+    }
+
+    while (index > 0) {
+        int parent = (index - 1) / 2;
+        tree[parent] = std::min(tree[2 * parent + 1], tree[2 * parent + 2]);
+        index = parent;
+    }
+} 
+
 std::vector<int> tournament_sort(std::vector<int>& list) {
     std::vector<int> tree = build_tree(list);
+    std::vector<int> result;
+
+    for (int i = 0; i < list.size(); i++) {
+        result.push_back(tree[0]);     
+        remove_winner(tree);          
+    }
+
+    return result;
 }
+
+void tim_insertion_sort(std::vector<int>& list, int start, int end) {
+    for (int i = start + 1; i < end; i++) {
+        int key = list[i];
+        int j = i - 1;
+        while (j >= start && list[j] > key) {
+            list[j + 1] = list[j];
+            j--;
+        }
+        list[j + 1] = key;
+    }
+}
+
+std::vector<std::pair<int, int>> extract_runs(const std::vector<int>& arr, int minrun) {
+    std::vector<std::pair<int, int>> runs;
+    int i = 0;
+    while (i < arr.size()) {
+        int start = i;
+        int end = i + 1;
+
+        if (end < arr.size() && arr[end] < arr[start]) {
+            while (end < arr.size() && arr[end] < arr[end - 1]) {
+                end++;
+            }
+
+        } 
+        
+        else {
+            // 오름차순 run
+            while (end < arr.size() && arr[end] >= arr[end - 1]) {
+                end++;
+            }
+        }
+
+        int run_len = end - start;
+
+        // run이 MINRUN보다 짧으면 늘리기 (삽입 정렬 대상)
+        if (run_len < minrun) {
+            int new_end = std::min(start + minrun, (int)arr.size());
+            // 삽입 정렬: insertion_sort(arr, start, new_end);
+            end = new_end;
+        }
+
+        // run 기록
+        runs.push_back({start, end});
+
+        // 다음 run 시작 지점
+        i = end;
+    }
+
+    return runs;
+}
+
+
+bool is_sorted(std::vector<std::pair<int, int>>& list) {
+    for(int i = 0; i < list.size() - 1; i++) {
+        if (list[i].first > list[i+1].first) {
+            std::cout << "error" << std::endl;
+            return false;
+        }
+    }
+    std::cout << "sorted!" << std::endl;
+    return true;
+}
+
+bool is_stable(std::vector<std::pair<int, int>>& list) {
+    for(int i = 0; i < list.size() - 1; i++) {
+        if(list[i].first == list[i+1].first) {
+            if (list[i].second > list[i+1].second) {
+                std::cout << "not stable" << std::endl;
+                return false;
+            }
+        }
+    }
+    std::cout << "stable" << std::endl;
+    return true;
+}
+
 int main() {
     int size;
     std::cin >> size;
-    std::vector<int> input = make_random_vector(size);
+    std::vector<std::pair<int, int>> input = make_random_vector(size);
 
     auto start = std::chrono::high_resolution_clock::now();
-    std::vector<int> result = library_sort(input);
+    heap_sort(input);
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration<double, std::milli>(end - start).count();
     std::cout << "Execution time: " << duration << " ms\n";
-    is_sorted(result);
+    is_sorted(input);
+    is_stable(input);
 
     return 0;
 }
